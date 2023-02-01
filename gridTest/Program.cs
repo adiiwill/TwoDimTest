0d9dcfe0917138370d22c8426db77c4617c7c665
adiiwill <gazdagadam02@gmail.com>
2023-02-01 19:47:28 +0100

    Add project files.

---
+++ gridTest/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace gridTest
+{
+    internal class Program
+    {
+        static void Print(tdArray array)
+        {
+            for (int i = 0; i < array.a; i++)
+            {
+                for (int j = 0; j < array.b; j++)
+                {
+                    Console.Write($"{array.arrayOfNumbers[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Generates the array as "test"
+            tdArray test = new tdArray(5, 5);
+
+            Console.WriteLine("TEST DEMO\n");
+            Console.WriteLine("===================================");
+
+            //Prints out the array (not included in the class)
+            Print(test);
+
+            Console.WriteLine("===================================");
+
+            // Inputs a number into the "number" variable
+            Console.Write("\nInsert a number from the list: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // The "nb" array will store the tdArray's neighbors. (check the Neighbor's function for more info)
+            int[] nb = test.Neighbors(number);
+
+            // Will write out the specified number's neighbors (0s are out of index indicators)
+            Console.WriteLine("\nIt's neighbors:");
+            Console.WriteLine($"\t{nb[0]}\t{nb[1]}\t{nb[2]}");
+            Console.WriteLine($"\t{nb[3]}\tN\t{nb[4]}");
+            Console.WriteLine($"\t{nb[5]}\t{nb[6]}\t{nb[7]}");
+        }
+    }
+}
---
+++ gridTest/tdArray.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gridTest
+{
+    internal class tdArray
+    {
+        public int[,] arrayOfNumbers;
+
+        public int a;
+        public int b;
+
+        public tdArray(int a, int b)
+        { 
+            arrayOfNumbers = new int[a, b];
+            this.a = a;
+            this.b = b;
+            FillArray();
+        }
+
+        public int[] Neighbors(int location)
+        {
+            // 1 2 3
+            // 4 x 5
+            // 6 7 8
+            return new int[] { TOP_LEFT(location), TOP(location), TOP_RIGHT(location), LEFT(location), RIGHT(location), BOTTOM_LEFT(location), BOTTOM(location), BOTTOM_RIGHT(location) };
+        }
+
+        public int Len
+        {
+            get
+            {
+                return arrayOfNumbers.Length;
+            }
+        }
+
+        #region SIDES
+        public int TOP_LEFT(int location)
+        {
+            try
+            {
+                return arrayOfNumbers[Find(location)[0] - 1, Find(location)[1] - 1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            }
+            
+        }
+        public int TOP(int location)
+        {
+            try
+            {
+                return arrayOfNumbers[Find(location)[0] - 1, Find(location)[1]];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            }
+        }
+        public int TOP_RIGHT(int location)
+        {
+            try
+            {
+                return arrayOfNumbers[Find(location)[0] - 1, Find(location)[1] + 1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            }
+        }
+        public int LEFT(int location)
+        {
+            try
+            {
+                return arrayOfNumbers[Find(location)[0], Find(location)[1] - 1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            }
+        }
+        public int RIGHT(int location)
+        {
+            try
+            {
+                return arrayOfNumbers[Find(location)[0], Find(location)[1] + 1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            }
+        }
+        public int BOTTOM_LEFT(int location)
+        {
+            try
+            {
+                return arrayOfNumbers[Find(location)[0] + 1, Find(location)[1] - 1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            }
+        }
+        public int BOTTOM(int location)
+        {
+            try
+            {
+                return arrayOfNumbers[Find(location)[0] + 1, Find(location)[1]];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            }
+        }
+        public int BOTTOM_RIGHT(int location)
+        {
+            try
+            {
+                return arrayOfNumbers[Find(location)[0] + 1, Find(location)[1] + 1];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return 0;
+            }
+        }
+        #endregion
+
+        #region PRIVATE
+        private int[] Find(int number)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    if (arrayOfNumbers[i, j] == number) return new int[] { i, j };
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+        private void FillArray()
+        {
+            Random rnd = new Random();
+
+            int rndNumber = 0;
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    while (true)
+                    {
+                        rndNumber = rnd.Next(1, Len + 1);
+                        if (!Has(rndNumber)) break;
+                    }
+
+                    arrayOfNumbers[i, j] = rndNumber;
+                }
+            }
+
+        }
+        private bool Has(int value)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    if (arrayOfNumbers[i, j] == value) return true;
+                }
+            }
+
+            return false;
+        }
+        #endregion
+    }
+}
